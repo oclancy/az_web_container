9dc29bb7ffc20503573c95e301df04f600f5379d
Oliver Clancy <oclancy@gmail.com>
2021-11-30 20:55:46 +0000

    add az credentials

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+
+const azureIdentity = require("@azure/identity");
+const appConfig = require("@azure/app-configuration");
+
+const credential = new azureIdentity.DefaultAzureCredential();
+// const client = new appConfig.AppConfigurationClient(
+//   endpoint, // ex: <https://<your appconfig resource>.azconfig.io>
+//   credential
+// );
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
