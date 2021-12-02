9c0ec5e3f9edb3431e7bb5d94ab96ba5d68ddbba
Oliver Clancy <oclancy@gmail.com>
2021-12-02 21:02:24 +0000

    add appconfig

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const appConfigUrl = process.env.APPCONFIG
 
-
+console.log('${appConfigUrl}')
 const azureIdentity = require("@azure/identity");
 const appConfig = require("@azure/app-configuration");
 
 const credential = new azureIdentity.DefaultAzureCredential();
-// const client = new appConfig.AppConfigurationClient(
-//   endpoint, // ex: <https://<your appconfig resource>.azconfig.io>
-//   credential
-// );
+const client = new appConfig.AppConfigurationClient(
+  appConfigUrl, // ex: <https://<your appconfig resource>.azconfig.io>
+  credential
+);
 
+(async () => {
+var port = await client.getConfigurationSetting({key:"BrightWebAppPort"})
+
+console.log('${appConfigUrl}')
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -19,3 +23,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+})()
